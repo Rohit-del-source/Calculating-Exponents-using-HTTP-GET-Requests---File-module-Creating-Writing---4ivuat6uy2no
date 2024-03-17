ac79ce9b30472196ec813b7f7277d5056ddc69e1
Rohit-del-source <rohitverma18158920@gmail.com>
2024-03-17 11:13:39 +0530

    initial

--- src/app.js
+++ src/app.js
@@ -11,8 +11,19 @@ const server = http.createServer((req, res) => {
       const obj = JSON.parse(chunks)
       const value1 = obj.num1;
       const value2 = obj.num2;
-
-      // Write code here to calculate power of a number
+      if(value1 < 1 || value2 < 0){
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        return res.end("The operation cannot be performed");
+      }
+      else if(  !value1 || !value2 && value2 !== 0 || !obj){
+          res.writeHead(400, {'Content-Type': 'text/plain'})
+          return res.end("Invalid input");
+      }
+      else{
+          let result = Math.pow(value1, value2);
+          res.writeHead(200,{'Content-Type':'plain/text'})
+          return res.end(`The result is ${result}`);
+      }
       
     });
     }
